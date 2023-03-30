a79a33236e72857e3549e2a74204675470f6cf2b
Izma <71832894+Izma810@users.noreply.github.com>
2023-03-30 17:24:51 +0530

    events done, nothing's responsive tho :')

--- components/Events.js
+++ components/Events.js
@@ -1,6 +1,60 @@
+import Image from 'next/image';
+import Event from '../public/img/events.svg';
+import SlantArrow from '../public/icons/slant_arrow.svg';
+
 function Events(){
     return(
         <section className='events-section'>
+            <div className='event-sec__img-div'>
+                <Image src={Event} alt='helo' className='img-div__img'/>
+            </div>
+            <div className='events-sec__text'>
+                <div className='etext__box'>
+
+                    <div className='box__mini-text'>
+
+                        <div className='mini-text__title'>
+                            <p className='title__bold'>Lorem Ipsum</p>
+                            <p className='title__date'>October 2, 2022</p>
+                        </div>
+
+                        <div className='mini-text__arrow'>
+                            <Image src={SlantArrow} height={40} width={40} alt='i sighed i need to clean again'/>
+                        </div>
+
+                    </div>
+
+                    <div className='mini-text__para'>
+
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non eleifend orci, ac dignissim dolor. Nunc feugiat erat purus, sed imperdiet mi placerat quis.
+
+                    </div>
+
+                </div>
+
+                <div className='etext__box'>
+
+                    <div className='box__mini-text'>
+
+                        <div className='mini-text__title'>
+                            <p className='title__bold'>Lorem Ipsum</p>
+                            <p className='title__date'>October 2, 2022</p>
+                        </div>
+
+                        <div className='mini-text__arrow'>
+                            <Image src={SlantArrow} height={40} width={40} alt='i sighed i need to clean again'/>
+                        </div>
+
+                    </div>
+
+                    <div className='mini-text__para'>
+
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non eleifend orci, ac dignissim dolor. Nunc feugiat erat purus, sed imperdiet mi placerat quis.
+                        
+                    </div>
+
+                </div>
+            </div>
         </section>
     )
 }
